faecb339211ab8789d953923835f3ebc046c7fe0
ashish goswami <ashish@dgraph.io>
2020-08-04 17:07:47 +0000

    Fixed list votes

--- routes/list_votes.js
+++ routes/list_votes.js
@@ -8,20 +8,13 @@ var data = ''
 exports.post = function (req, res, next) {
 
   console.log('List results of current poll.');
+  pollId = 'activePoll_' + req.body.channel_id;
+  dbActions.getPoll(pollId, printPoll);
 
-  /*
-   * Print active poll results.
-   * TBD: Needs to be refactored if we want to use this route
-   */
-  // dbActions.getActivePollId(fetchActivePoll);
-  // function fetchActivePoll(pollId) {
-  //   console.log('Fetching active poll: ' + pollId);
-  //   dbActions.getPoll(pollId, printPoll);
-  // }
-  // function printPoll(data) {
-  //   slackRes = 'Here are the current votes: \n ' + tally.printPoll(JSON.parse(data));
-  //   console.log('printPoll: ' + slackRes);
-  //   res.json({text: slackRes});
-  // }
+  function printPoll(data) {
+    slackRes = 'Here are the current votes: \n ' + tally.printPoll(JSON.parse(data));
+    console.log('printPoll: ' + slackRes);
+    res.json({text: slackRes});
+  }
 
 };
